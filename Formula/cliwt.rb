7285bb1a9b7ad5b6fa65a744b59d1a302742da97
HenryLoM <itcubegames@gmail.com>
2025-11-14 20:01:25 +0300

    Update: v1.0.1

--- Formula/cliwt.rb
+++ Formula/cliwt.rb
@@ -1,23 +1,13 @@
 class Cliwt < Formula
-  desc "Cute terminal waifu tamagotchi for you"
+  desc "Cute terminal waifu tamagotchi"
   homepage "https://github.com/HenryLoM/CliWaifuTamagotchi"
-  url "https://github.com/HenryLoM/CliWaifuTamagotchi/releases/download/v1.0.0/cliwt-v1.0.0.tar.gz"
-  sha256 "589175e2a46a00d85a4bf2811ae5aed54ad25e5241cd2c7e6a006eede48e8d5b"
-  version "1.0.0"
+  url "https://github.com/HenryLoM/CliWaifuTamagotchi/releases/download/v1.0.1/cliwt-v1.0.1-macos-arm64.tar.gz"
+  sha256 "2e34e203d0885716f19f99424edbf1fdfb9a370f3b0ff06f4690e4d418e65ac3"
+  version "1.0.1"
 
   def install
-    # Install binary and rename it to avoid conflict with wrapper
-    bin.install "cliwt" => "cliwt.real"
-
-    # Install asset directories
-    prefix.install "assets", "ascii-arts", "utils"
-
-    # Wrapper script to run binary from the prefix directory
-    (bin/"cliwt").write <<~EOS
-      #!/bin/bash
-      cd "#{prefix}" || exit 1
-      exec "#{bin}/cliwt.real" "$@"
-    EOS
+    # Install binary from the archive to bin/
+    bin.install "cliwt"
   end
 
   test do
